8ccff65bc58c00eb068081dfcc8cb90177b5f8d4
danielle lane <daniellelane1821@gmail.com>
2019-10-10 15:15:08 -0500

    finished second exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,21 @@
 USE codeup_test_db;
+
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+
+SELECT release_date FROM albums WHERE id = 48;
+
+
+SELECT genre FROM albums WHERE artist = 'Nirvana' AND name = 'Nevermind';
+
+
+SELECT * FROM albums WHERE release_date > 1989 AND release_date < 2000;
+
+
+SELECT * FROM albums WHERE sales <= 20;
+
+
+SELECT * FROM albums WHERE genre = 'Rock';
+
+
+
