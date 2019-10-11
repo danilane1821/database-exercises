a31a57ccf1dadb5908c44c145bf8665f777d44b7
danielle lane <daniellelane1821@gmail.com>
2019-10-11 08:35:26 -0500

    finished

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,3 +1,8 @@
 USE codeup_test_db;
 
-SELECT name, release_date, FROM albums
+SELECT name, release_year from albums where release_year > 1991;
+DELETE from albums where release_year > 1991;
+SELECT name, genre from albums where genre = 'Disco';
+DELETE from albums where genre = 'Disco';
+select name, artist from albums where artist = 'Metallica';
+DELETE from albums where artist = 'Metallica';
