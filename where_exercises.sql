6fc4486fb3c2845b3fd8005673b48f18a36acc03
danielle lane <daniellelane1821@gmail.com>
2019-10-11 14:01:30 -0500

    finished with walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,44 +1,45 @@
 USE employees;
 
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE (first_name = 'Irena') Or (first_name = 'Vidya') Or (first_name = 'Maya');
 
-
-ELECT emp_no, first_name, last_name, GENDER
+SELECT *
 FROM employees
-WHERE gender = 'M' AND (first_name = 'Irena') Or Gender = 'M' AND (first_name = 'Vidya') Or Gender = 'M' AND (first_name = 'Maya');
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya') AND gender = 'M';
 
 
-
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE last_name LIKE ('E%');
 
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE last_name LIKE ('E%')
 OR last_name like('%E');
 
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE last_name LIKE ('E%')
 AND last_name like('%E');
+# or WHERE last_name LIKE ('E%E');
 
 
 
-SELECT emp_no, first_name, last_name, hire_date
+SELECT *
 FROM employees
-WHERE hire_date like ('199%');
+WHERE hire_date like ('199%')
 AND birth_date like ('%12-25');
 
 
 
-SELECT emp_no, first_name, last_name, birth_date
+SELECT first_name, last_name, birth_date
 FROM employees
 WHERE birth_date like ('%12-25');
 
