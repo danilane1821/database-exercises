72cf331e7dfb73cc0fc67b290904b35cb61796a3
danielle lane <daniellelane1821@gmail.com>
2019-10-15 11:17:18 -0500

    finished exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,36 @@
+use employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE ('E%E')
+group by last_name;
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE ('E%E')
+group by last_name;
+
+
+SELECT last_name
+FROM employees
+WHERE last_name like ('%q%')
+AND last_name NOT LIKE ('%qu%')
+group by last_name;
+
+
+SELECT last_name, count(*)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+group by last_name
+ORDER BY COUNT(*);
+
+
+SELECT COUNT(FIRST_NAME), gender
+FROM employees
+WHERE (first_name = 'Irena') Or (first_name = 'Vidya') Or (first_name = 'Maya')
+group by gender;
