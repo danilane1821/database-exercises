f949fee2743bf2bc4a546ceb036bdd5ff3061855
danielle lane <daniellelane1821@gmail.com>
2019-10-15 14:09:18 -0500

    finished with walkthrough

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -4,7 +4,7 @@ SELECT CONCAT(LAST_name, ' ', FIRST_name) AS full_name
 FROM employees
 LIMIT 10;
 
-SELECT CONCAT(LAST_name, ' ', FIRST_name) AS full_name
+SELECT CONCAT(LAST_name, ' ', FIRST_name) AS full_name, birth_date AS "DOB"
 FROM employees
 LIMIT 10;
 
--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,10 +10,10 @@ WHERE last_name LIKE ('E%E')
 group by last_name;
 
 
-SELECT first_name, last_name
+SELECT last_name, first_name
 FROM employees
 WHERE last_name LIKE ('E%E')
-group by last_name;
+group by last_name, first_name;
 
 
 SELECT last_name
